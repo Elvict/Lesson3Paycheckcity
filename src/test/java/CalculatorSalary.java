ce5b1d05a49a076d0e7a78276cd6dca910d963e2
ER <elvict@gmail.com>
2016-08-22 00:28:33 +0300

    Initial commit

---
+++ src/test/java/CalculatorSalary.java
@@ -0,0 +1,74 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+
+public class CalculatorSalary {
+    private String baseUrl = "http://www.paycheckcity.com/";
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        //       baseUrl = "https://trello.com/login/";
+        //       System.setProperty("webdriver.ie.driver", "C:\\Java\\Selenium\\IEDriverServer.exe");
+        //       WebDriver driver = new InternetExplorerDriver();
+        driver = new FirefoxDriver();
+//        driver.get("http://www.paycheckcity.com/");
+        driver.get(baseUrl+"calculator/salary/");
+        driver.manage().timeouts().implicitlyWait(101,TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void fillParametersAndCalculateSalary() throws Exception {
+        driver.findElement(By.id("calcDate")).clear();
+        driver.findElement(By.id("calcDate")).sendKeys("2016-08-31");
+        new Select(driver.findElement(By.id("state"))).selectByVisibleText("California");
+        driver.findElement(By.id("generalInformation.grossPayAmount")).clear();
+        driver.findElement(By.id("generalInformation.grossPayAmount")).sendKeys("10000");
+        new Select(driver.findElement(By.id("generalInformation.grossPayMethodType"))).selectByVisibleText("Pay Per Period");
+        new Select(driver.findElement(By.id("generalInformation.payFrequencyType"))).selectByVisibleText("Monthly");
+  //      driver.findElement(By.id("generalInformation.exemptFederal1")).click();
+        if ( !driver.findElement(By.id("generalInformation.exemptFederal1")).isSelected() )
+        {
+            driver.findElement(By.id("generalInformation.exemptFederal1")).click();
+        }
+
+    //    driver.findElement(By.id("calculate")).click();
+        driver.get("http://some_url");
+        WebElement dynamicElement = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.id("dynamicElement_id")));
+
+        driver.get(baseUrl+"calculator/salary/result");
+        driver.manage().timeouts().implicitlyWait(101,TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        String expectedResult = "$8,387.79";
+        String actualResult = driver.findElement(By.id("fn_o1_1")).getAttribute("value");
+        while(actualResult.contains(Character.toString((char)160))) {
+            String replace = actualResult.replace(Character.toString((char)160), "");
+            actualResult=replace;
+        }
+
+        assertEquals(actualResult, expectedResult);
+//        (new WebDriverWait(driver,10))
+//                .until(ExpectedConditions.textToBePresentInElement(result,expectedResult));
+        assertEquals(actualResult,expectedResult);
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
