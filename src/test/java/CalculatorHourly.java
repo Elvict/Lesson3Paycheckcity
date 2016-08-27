9a18b4d30fe437902aef410d3fcd81372e03424d
ER <elvict@gmail.com>
2016-08-27 23:44:28 +0300

    Updated Calculator Hourly an Salary - Finished

--- src/test/java/CalculatorHourly.java
+++ src/test/java/CalculatorHourly.java
@@ -2,41 +2,57 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.util.concurrent.TimeUnit;
+import static org.testng.Assert.assertEquals;
 
 public class CalculatorHourly {
     private String baseUrl = "http://www.paycheckcity.com/";
     private WebDriver driver;
-//    DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
-//WebElement source = driver.findElement(By.xpath(".//a[text()=\"Second card\" and @class=\"list-card-title js-card-name\"]"));
-//    WebElement target = driver.findElement(By.xpath("//textarea[contains(text(),'Advanced')]"));
+    private String expectedResult = "8640";
 
-    @Test
-    public void fillParametersAndCalculateHourly() throws Exception {
+    @BeforeMethod
+    public void setUp() throws Exception {
         driver = new FirefoxDriver();
         driver.get("http://www.paycheckcity.com/");
         driver.get(baseUrl+"calculator/hourly/");
+        System.out.println(baseUrl+"calculator/hourly/");
         driver.manage().timeouts().implicitlyWait(101,TimeUnit.SECONDS);
         driver.manage().window().maximize();
+    }
 
+    @Test
+    public void fillParametersAndCalculateHourly() throws Exception {
+ //       String expectedResult = "8640";
         driver.findElement(By.id("calcType.rates0.payRate")).clear();
         driver.findElement(By.id("calcType.rates0.payRate")).sendKeys("50");
         driver.findElement(By.id("calcType.rates0.hours")).clear();
-        driver.findElement(By.id("calcType.rates0.payRate")).sendKeys("160");
-        driver.findElement(By.id("addRate")).click();
-        WebElement dynamicElement = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("\\\\span[@class=\"resultData\"]\\..\\..\\..\\div[@title=\"Net Pay\"]")));
+        driver.findElement(By.id("calcType.rates0.hours")).sendKeys("160");
+        driver.findElement(By.id("addRate_label")).click();
+        WebElement dynamicElement1 = (new WebDriverWait(driver, 101))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("calcType.rates1.payRate")));
+        driver.findElement(By.id("calcType.rates1.payRate")).clear();
+        driver.findElement(By.id("calcType.rates1.payRate")).sendKeys("80");
+        driver.findElement(By.id("calcType.rates1.hours")).clear();
+    //    driver.findElement(By.id("generalInformation.grossPayAmount")).clear();
+        driver.findElement(By.id("calcType.rates1.hours")).sendKeys("8");
+        driver.findElement(By.id("generalInformation.grossPayAmount")).click();
 
- //       source = driver.findElement(By.xpath(".//a[text()=\"Second card\" and @class=\"list-card-title js-card-name\"]"));
+        String actualResult
+                = driver.findElement(By.id("generalInformation.grossPayAmount")).getAttribute("value");
+        System.out.println("expectedResult = "+expectedResult);
+        System.out.println("actualResult = "+actualResult);
+        assertEquals(actualResult, expectedResult);
 
+        }
+    @AfterMethod
+    public void tearDown() throws Exception {
+        driver.quit();
     }
-
 }
 
 
